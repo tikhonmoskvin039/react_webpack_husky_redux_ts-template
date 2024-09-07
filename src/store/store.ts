32f126e2f11aa7ee24cdeeec7c7d8a3c65565468
Tikhon Moskvin <tihonmoskvin@mail.ru>
2024-09-07 16:28:22 +0300

    fix: rules syntaxis for eslint 9.9.9

--- src/store/store.ts
+++ src/store/store.ts
@@ -15,5 +15,9 @@ sagaMiddleware.run(rootSaga);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type TRootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+
+/**
+ * Inferred type: {posts: PostsState, comments:
+ * CommentsState, users: UsersState}
+ */
 export type TAppDispatch = typeof store.dispatch;
